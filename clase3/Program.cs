99aaeaef177973284a1f11b99a5fad246749d989
Ivana Yael Currá <ivanayael@hotmail.com>
2020-05-26 22:15:22 -0300

    Tercera Clase

---
+++ clase3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace clase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("View Layer");
+            Console.WriteLine("My second app in c# netCore");
+
+            Pizza p1 = createPizza("mold", "tomato", "muzzarella", 20.5);
+        }
+
+        //builder pattern
+        public static Pizza createPizza(string MazzaType, string ingredient1, string ingredient2, double size){
+            Pizza pizza = new Pizza();
+
+            pizza.MazzaType = MazzaType;
+            pizza.ingredient1 = ingredient1;
+            pizza.ingredient2 = ingredient2;
+            pizza.size = size;
+
+            return pizza;
+        }
+    }
+}
---
+++ clase3/modelo/Pizza.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace clase3
+{
+     public class Pizza
+     {
+          public string MazzaType {get; set; }
+
+          public double size {get; set; }
+
+          public string ingredient1 {get; set; }
+
+          public string ingredient2 {get; set; }
+
+
+     }
+
+}
