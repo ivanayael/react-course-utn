762dec8aca6a02679afa0b6d65267d0f6791fac1
Ivana Yael Curra <ivanayael@hotmail.com>
2020-06-03 13:36:18 -0300

    clase 5

---
+++ clase5vectores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase5vectores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
